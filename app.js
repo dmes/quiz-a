ecd4e03b3a752988ea9011403bc7ff940bd80af9
Pavononer17 <arturo.pavon.estrade@alumnos.upm.es>
2015-05-19 17:53:35 +0000

    12.5

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var partials = require('express-partials');
-
-var routes = require('./routes/index');
-var creditos = require('./routes/author');
-
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -32,6 +18,7 @@ app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -39,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/author', creditos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,8 +43,7 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: {},
-            errors: []
+            error: err
         });
     });
 }
@@ -69,8 +54,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {},
-        errors: []
+        error: {}
     });
 });
 
