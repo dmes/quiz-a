cbe21c38e2074043fe2d09da5158cae5e51fa4cc
Pavononer17 <arturo.pavon.estrade@alumnos.upm.es>
2015-05-19 17:52:22 +0000

    12.4

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var partials = require('express-partials');
+
+var routes = require('./routes/index');
+var creditos = require('./routes/author');
+
+var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,7 +32,6 @@ app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -26,6 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/author', creditos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,12 +50,14 @@ app.use(function(req, res, next) {
 
 // error handlers
 
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err,
+            error: {},
             errors: []
         });
     });
@@ -57,5 +73,6 @@ app.use(function(err, req, res, next) {
         errors: []
     });
 });
+
 
 module.exports = app;
